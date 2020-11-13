77bf0b6762a9cb27ed35f360092ab0bdd5183873
yuanxuezhe <787210492@qq.com>
2020-11-13 21:34:25 +0800

    tools

---
+++ cpp/ys_public/ys_tools.h
@@ -0,0 +1,14 @@
+/*
+ *@name:        ys_tools.h
+ *@description: tools
+ *@author:      yuanshuai
+ *@mail:        446968454@qq.com
+ *@date:        2020-11-13 21:33:54
+ *@version:     V1.0.0
+*/
+
+#ifndef YS_TOOLS_H 
+#define YS_TOOLS_H 
+
+
+#endif	// YS_TOOLS_H
