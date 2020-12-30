c1bbe55a395cfeeb7dbfd36dbd3b4b7facd1d897
yuanxuezhe <787210492@qq.com>
2020-12-30 08:40:14 +0800

    commit

---
+++ cpp/algorithm/tree/BinaryTree/BinaryTree.h
@@ -0,0 +1,73 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-30 07:17:57
+ * @LastEditTime: 2020-12-30 08:39:44
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /workspace/cpp/algorithm/tree/BinaryTree/BinaryTree.h
+ */
+#ifndef __BINARYTREE_h__
+#define __BINARYTREE_h__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//https://zhuanlan.zhihu.com/p/87589946
+// 定义二叉树结点
+struct Node {
+    int data;
+    struct Node* pLChild;
+    struct Node* pRChild;
+} Node;
+
+// 创建一个二叉树节点
+struct Node* createNode(int value) {
+    struct  Node* pNode = (struct  Node*)malloc(sizeof(struct Node));
+    pNode->data = value;
+    pNode->pLChild = NULL;
+    pNode->pRChild = NULL;
+    return pNode;
+}
+
+// 数据插入二叉树
+struct Node* addNode(int value, struct Node*pNode) {
+    if (pNode == NULL)
+    {
+         return  createNode(value);
+    }
+
+    if (value == pNode->data)
+    {
+        return pNode;
+    }
+    
+    if (value < pNode->data)
+    {
+        if (pNode->pLChild == NULL)
+        {
+            pNode->pLChild = createNode(value);
+            return pNode;
+        }
+        else
+        {
+            return addNode(value, pNode->pLChild);
+        }
+        
+        
+    }
+
+    if (value > pNode->data)
+    {
+        if (pNode->pRChild == NULL)
+        {
+            pNode->pRChild = createNode(value);
+            return pNode;
+        }
+        else
+        {
+            return addNode(value, pNode->pRChild);
+        }
+    }
+}
+ 
+#endif
---
+++ cpp/algorithm/tree/BinaryTree/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-30 07:51:22
+ * @LastEditTime: 2020-12-30 08:37:21
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /workspace/cpp/algorithm/tree/BinaryTree/main.cpp
+ */
+
+#include "stdio.h"
+#include "BinaryTree.h"
+
+int main(int argc, char const *argv[])
+{
+    struct Node* pnode;
+    int a[] = {9,1,2,22,65,32,53,3,4,5,6,7};
+    for (size_t i = 0; i < sizeof(a)/sizeof(int); i++)
+    {
+        pnode = addNode(a[i],NULL);
+    }
+    
+    return pnode->data;
+    return 0;
+}
