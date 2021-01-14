d5e7d155a0b83755d07456bb481feb9ae571c6fa
yuanxuezhe <787210492@qq.com>
2021-01-15 00:58:40 +0800

    react demo

--- nodejs/react1/src/index.js
+++ nodejs/react1/src/index.js
@@ -3,11 +3,60 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PropTypes from 'prop-types'
 
+var names = ['Alice', 'Emily', 'Kate'];
+var arr = [
+  <h1>Hello world!</h1>,
+  <h2>React is awesome</h2>,
+];
+
+class HelloMessage extends React.Component{
+  render() {
+    return <h1>Hello {this.props.name}</h1>;
+  }
+};
+
+class NotesList extends React.Component{
+  render() {
+    return (
+      <ol>
+      {
+        React.Children.map(this.props.children, function (child) {
+          return <li>{child}</li>;
+        })
+      }
+      </ol>
+    );
+  }
+};
+
+class MyTitle extends React.Component{
+  render() {
+     return <h1> {this.props.title} </h1>;
+   }
+};
+
+MyTitle.propTypes = {
+  title: PropTypes.string.isRequired, 
+  //title:React.PropTypes.string.isRequired
+}
+
+var data = 123;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyTitle title={data} />,
+  // <NotesList>
+  //   <span>hello</span>
+  //   <span>world</span>
+  //   <span>33</span>
+  //   <span>33</span>
+  //   <span>wo22rld</span>
+  //   <span>qq</span>
+  //   <span>ee</span>
+  // </NotesList>,
+  //  <HelloMessage name="John12313" />,
+    //<div>{arr}</div>,
+    //<h1>Hello, world!</h1>,
   document.getElementById('root')
 );
 
